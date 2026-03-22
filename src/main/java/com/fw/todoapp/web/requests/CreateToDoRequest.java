3c2f463478cca3e35df4d215421850f676215941
fw <thatgloomyafternoon@gmail.com>
2026-03-22 10:02:32 +0700

    update

---
+++ src/main/java/com/fw/todoapp/web/requests/CreateToDoRequest.java
@@ -0,0 +1,28 @@
+package com.fw.todoapp.web.requests;
+
+import java.util.UUID;
+
+public class CreateToDoRequest {
+
+  private UUID tagId;
+  private String task;
+
+  public CreateToDoRequest() {}
+
+  public UUID getTagId() {
+    return tagId;
+  }
+
+  public void setTagId(UUID tagId) {
+    this.tagId = tagId;
+  }
+
+  public String getTask() {
+    return task;
+  }
+
+  public void setTask(String task) {
+    this.task = task;
+  }
+
+}
