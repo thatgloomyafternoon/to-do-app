c906f994a83536f6248c0757a69606c8f99f66f4
fw <thatgloomyafternoon@gmail.com>
2026-03-22 10:02:29 +0700

    update

---
+++ src/main/java/com/fw/todoapp/web/requests/CreateTagRequest.java
@@ -0,0 +1,17 @@
+package com.fw.todoapp.web.requests;
+
+public class CreateTagRequest {
+
+  private String name;
+
+  public CreateTagRequest() {}
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
